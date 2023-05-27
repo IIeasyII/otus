aa65a656d44db46c7c6905f7a9beca76a4c88f8f
easy1i <zavolokin.iv@gazprom-neft.ru>
2023-05-27 22:27:21 +0300

    Expression calculation

--- Program.cs
+++ Program.cs
@@ -5,100 +5,148 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var list = new List<int>();
-        var arrayList = new ArrayList();
-        var linkedList = new LinkedList<int>();
-        
-        #region Insert performance
-        System.Console.WriteLine($"Время на заполнение коллекций");
-        Stopwatch stopwatch = new Stopwatch();
+        System.Console.WriteLine("a*x^2+b*x+c=0");
+        Dictionary<char, int> arguments;
+        while (!TryReadParameters(out arguments)) ;
 
-        stopwatch.Start();
-        for(var i = 0; i < 1000000; i++)
-            list.Add(i);
-        stopwatch.Stop();
+        var quadraticEquation = new QuadraticEquation(arguments);
 
-        System.Console.WriteLine($"'List<int>' - {stopwatch.ElapsedMilliseconds} milliseconds");
-        
-        stopwatch.Reset();
-        stopwatch.Start();
-        for(var i = 0; i < 1000000; i++)
-            arrayList.Add(i);
-        stopwatch.Stop();
-
-        System.Console.WriteLine($"'ArrayList' - {stopwatch.ElapsedMilliseconds} milliseconds");
-
-        stopwatch.Reset();
-        stopwatch.Start();
-        for(var i = 0; i < 1000000; i++)
-            linkedList.AddLast(i);
-        stopwatch.Stop();
-
-        System.Console.WriteLine($"'LinkedList<int>' - {stopwatch.ElapsedMilliseconds} milliseconds");
-        #endregion
-
-        #region Search element
-        System.Console.WriteLine($"Поиск элемента коллекций");
-        stopwatch.Reset();
-        stopwatch.Start();
-        var element = list[496753];
-        stopwatch.Stop();
-        System.Console.WriteLine($@"'List<int>' если брать по индексу, то операция выполняется за одну команду, счет идет на наносекунды и скорее всего зависимости от мощности машины {stopwatch.ElapsedMilliseconds} milliseconds");
-
-        stopwatch.Reset();
-        stopwatch.Start();
-        element = list.Find(x => x == 496753);
-        stopwatch.Stop();
-
-        System.Console.WriteLine($@"'List<int>' method 'Find': {stopwatch.ElapsedMilliseconds} milliseconds");
-
-        stopwatch.Reset();
-        stopwatch.Start();
-        element = list.BinarySearch(496753);
-        stopwatch.Stop();
-
-        System.Console.WriteLine($@"'List<int>' method 'BinarySearch': {stopwatch.ElapsedMilliseconds} milliseconds");
-
-        stopwatch.Reset();
-        stopwatch.Start();
-        element = arrayList.BinarySearch(496753);
-        stopwatch.Stop();
-
-        System.Console.WriteLine($@"'ArrayList' method 'BinarySearch': {stopwatch.ElapsedMilliseconds} milliseconds");
-        #endregion
-
-        #region Element % 777
-        System.Console.WriteLine($"Вывод времени выполнения поиска всех элементов деления на 777 без остатка");
-        stopwatch.Reset();
-        stopwatch.Start();
-        foreach(var item in list)
+        try
         {
-            if(item % 777 != 0) continue;
-            System.Console.WriteLine(item);
+            var result = quadraticEquation.Calculate();
+            System.Console.WriteLine(result);
         }
-        stopwatch.Stop();
-        System.Console.WriteLine($"'List<int>' %777: {stopwatch.ElapsedMilliseconds} milliseconds");
-        
-        stopwatch.Reset();
-        stopwatch.Start();
-        foreach(int item in arrayList)
+        catch (NoEquationRootsException ex)
         {
-            if(item % 777 != 0) continue;
-            System.Console.WriteLine(item);
+            FormatData(ex.Message, Severity.Warning, null!);
         }
-        stopwatch.Stop();
-        System.Console.WriteLine($"'ArrayList' %777: {stopwatch.ElapsedMilliseconds} milliseconds");
 
-        stopwatch.Reset();
-        stopwatch.Start();
-        foreach(int item in linkedList)
-        {
-            if(item % 777 != 0) continue;
-            System.Console.WriteLine(item);
-        }
-        stopwatch.Stop();
-        System.Console.WriteLine($"'LinkedList<int>' %777: {stopwatch.ElapsedMilliseconds} milliseconds");
-        #endregion
         Console.ReadKey();
     }
+
+    private static bool TryReadParameters(out Dictionary<char, int> arguments)
+    {
+        arguments = new();
+        var parameters = new Dictionary<char, string>()
+        {
+            {'a', string.Empty},
+            {'b', string.Empty},
+            {'c', string.Empty},
+        };
+
+        //read parameter from console
+        foreach (var item in parameters.Keys)
+        {
+            var message = $"Введите значение {item}:";
+            System.Console.WriteLine(message);
+            var input = Console.ReadLine();
+            parameters[item] = input;
+        }
+
+        try
+        {
+            foreach (var item in parameters)
+            {
+                var parameter = item.Key;
+                var value = item.Value;
+
+                if (!int.TryParse(value, out var number))
+                    continue;
+
+                parameters.Remove(parameter);
+                arguments.Add(parameter, number);
+            }
+            if (parameters.Keys.Count > 0)
+                throw new FormatException("Can't parse parameters");
+        }
+        catch (FormatException ex)
+        {
+            FormatData(ex.Message, Severity.Error, parameters);
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void FormatData(string message, Severity severity, IDictionary data)
+    {
+        switch (severity)
+        {
+            case Severity.Error:
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    foreach (var key in data.Keys)
+                        System.Console.WriteLine($"{key} - {data[key]}");
+                    break;
+                }
+            case Severity.Warning:
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    System.Console.WriteLine(message);
+                    break;
+                }
+        }
+        Console.ResetColor();
+    }
+
+    enum Severity
+    {
+        Warning,
+        Error
+    }
+}
+
+internal class NoEquationRootsException : Exception
+{
+    public NoEquationRootsException(string message) : base(message)
+    {
+
+    }
+}
+
+internal class QuadraticEquation
+{
+    private readonly Dictionary<char, int> _arguments;
+
+    public QuadraticEquation(Dictionary<char, int> arguments)
+    {
+        _arguments = arguments;
+    }
+
+    public string Calculate()
+    {
+        var a = _arguments['a'];
+        var b = _arguments['b'];
+        var c = _arguments['c'];
+
+        var discriminant = GetDiscriminant(a, b, c);
+        var answer = string.Empty;
+
+        switch (discriminant)
+        {
+            case var expression when discriminant < 0:
+                {
+                    throw new NoEquationRootsException("Вещественных значений не найдено.");
+                }
+            case var expression when discriminant == 0:
+                {
+                    var x = (-b + Math.Sqrt(discriminant)) / 2 * a;
+                    answer = $"x = {x}";
+                    break;
+                }
+            case var expression when discriminant > 0:
+                {
+                    var x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
+                    var x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
+                    answer = $"x1 = {x1}, x2 = {x2}";
+                    break;
+                }
+        }
+
+        return answer;
+    }
+
+    private double GetDiscriminant(int a, int b, int c) => Math.Pow(b, 2) - 4 * a * c;
 }
