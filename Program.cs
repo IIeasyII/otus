bc37505fb639ef6126b2e86d00c2d421b15091bc
easy1i <zavolokin.iv@gazprom-neft.ru>
2023-06-24 00:49:45 +0300

    Binary tree, order and find

--- Program.cs
+++ Program.cs
@@ -1,86 +1,145 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var result1 = FibonacciRecursive(10);
-        System.Console.WriteLine("FibonacciRecursive=" + result1);
-        var result2 = FibonacciRecursive(10);
-        System.Console.WriteLine("FibonacciLoop=" + result2);
-
-        Stopwatch stopwatch = new Stopwatch();
-
-        #region 5
-        var n = 5;
-        stopwatch.Start();
-        FibonacciRecursive(n);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciRecursive({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-
-        stopwatch.Start();
-        FibonacciLoop(n);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciLoop({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-        #endregion
-
-        #region 10
-        n = 10;
-        stopwatch.Start();
-        FibonacciRecursive(n);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciRecursive({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-
-        stopwatch.Start();
-        FibonacciLoop(10);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciLoop({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-        #endregion
-
-        #region 20
-        n = 20;
-        stopwatch.Start();
-        FibonacciRecursive(n);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciRecursive({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-
-        stopwatch.Start();
-        FibonacciLoop(n);
-        stopwatch.Stop();
-        System.Console.WriteLine($"FibonacciLoop({n})={stopwatch.ElapsedTicks} ticks" );
-        stopwatch.Reset();
-        #endregion
-    }
-
-    private static int FibonacciRecursive(int n)
-    {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-
-        return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
-    }
-
-    private static double FibonacciLoop(int n)
-    {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-
-        int result = 2;
-        var current = 1;
-        var previous = 2;
-
-        for(int i = 0; i < n; i++)
+        while (true)
         {
-            result = current + previous;
-            previous = current;
-            current = result;
+            System.Console.WriteLine("Введите дерево:");
+            BinaryTree binaryTree = new BinaryTree();
+            while (true)
+            {
+                var name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name)) break;
+
+                decimal.TryParse(Console.ReadLine(), out var salary);
+
+                var employee = new EmployeeInfo(name, salary);
+                binaryTree.Add(employee);
+            }
+
+            var sortedEmployeers = binaryTree.SymmetricTraversal();
+            foreach (var item in sortedEmployeers)
+                System.Console.WriteLine($"{item.Name}-{item.Salary}");
+
+            var isFind = true;
+            while (isFind)
+            {            
+                FindInBinaryTree(binaryTree);
+                System.Console.WriteLine("0 - создать новое дерево");
+                System.Console.WriteLine("1 - новый поиск");
+
+                if (Console.ReadLine() == "0")
+                    isFind = false;
+            }
+        }
+    }
+
+    private static void FindInBinaryTree(BinaryTree binaryTree)
+    {
+        System.Console.WriteLine("Поиск по зарплате. Введите размер зарплаты:");
+        var find = Console.ReadLine();
+        if (find is null) return;
+
+        var salary = decimal.Parse(find);
+
+        var employee = binaryTree.Find(x => x.Salary == salary);
+        if (employee is null)
+            System.Console.WriteLine("Такой сотрудник не найден");
+        else
+            System.Console.WriteLine(employee.Name);
+    }
+}
+
+internal class BinaryTree
+{
+    public Node? Root { get; set; }
+
+    public bool Add(EmployeeInfo employee)
+    {
+        Node before = null!;
+        Node? after = Root;
+
+        while (after != null)
+        {
+            before = after;
+            if (employee.Salary < after.EmployeeInfo.Salary)
+            {
+                after = after.LeftNode;
+            }
+            else if (employee.Salary > after.EmployeeInfo.Salary)
+            {
+                after = after.RightNode;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        return result;
+        Node node = new Node(employee);
+        if (Root == null)
+            Root = node;
+        else
+        {
+            if (employee.Salary < before.EmployeeInfo.Salary)
+                before.LeftNode = node;
+            if (employee.Salary > before.EmployeeInfo.Salary)
+                before.RightNode = node;
+        }
+
+        return true;
+    }
+
+    public EmployeeInfo? Find(Func<EmployeeInfo, bool> filter)
+    {
+        return SymmetricTraversal().FirstOrDefault(filter);
+    }
+
+    public IEnumerable<EmployeeInfo> SymmetricTraversal()
+    {
+        var stack = new Stack<Node>();
+        var node = Root;
+
+        while (stack.Count > 0 || node is not null)
+        {
+            if (node is not null)
+            {
+                stack.Push(node);
+                node = node.LeftNode;
+            }
+            else
+            {
+                node = stack.Pop();
+                yield return node.EmployeeInfo;
+                node = node.RightNode;
+            }
+        }
+    }
+}
+
+internal class Node
+{
+    public Node? LeftNode { get; set; }
+    public Node? RightNode { get; set; }
+    public EmployeeInfo EmployeeInfo { get; set; }
+
+    public Node(EmployeeInfo root)
+    {
+        EmployeeInfo = root;
+    }
+}
+
+internal class EmployeeInfo
+{
+    public string Name { get; }
+    public decimal Salary { get; }
+
+    public EmployeeInfo(string name, decimal salary)
+    {
+        Name = name;
+        Salary = salary;
     }
 }
